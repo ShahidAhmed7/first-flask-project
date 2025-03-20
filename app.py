760072123083ad2ed60f7229bcfbf947049e4297
Kazi Shahid Ahmed Galib <ahmedshahid2929@gmail.com>
2025-03-20 20:38:14 +0600

    add data driven features

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+from database import get_job_list
 
 app = Flask(__name__)
 
+job_list = get_job_list()
+
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html',jobs = job_list)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(job_list)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+import pymysql
+
+# Get the absolute path of the .env file
+from pathlib import Path
+dotenv_path = Path(__file__).resolve().parent / ".env"
+
+# Load environment variables
+load_dotenv(dotenv_path)
+
+# Get DB credentials
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Create Database URL
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create Engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_job_list():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+
+        result_all = result.all()
+
+        jobs_list = [dict(row._mapping) for row in result_all]
+        return jobs_list
+
+
